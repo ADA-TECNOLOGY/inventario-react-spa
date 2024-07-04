27b49182e76e6d341a507e9f2d8987976bee9bdb
Anna Vitória <anna.vitoria@intraversa.com>
2024-07-04 15:00:10 -0300

    fix: header

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,6 +2,6 @@ import { Box } from "@chakra-ui/react";
 
 export default function Home(){
     return (
-        <Box bg={'white'} height={'500PX'}>Home</Box>
+        <Box bg={'white'} height={'800px'}>Home</Box>
     )
 }
