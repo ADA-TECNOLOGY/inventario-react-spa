6bbca4eaf30492d67d75f9688d7482f10a614b3a
Arlyanne <arlyanne.lima.ferro@gmail.com>
2024-07-29 21:36:32 -0300

    criado model para telas criadas

---
+++ src/model/Address.model.ts
@@ -0,0 +1,30 @@
+export class AddressModel {
+  id: number;
+  street: string;
+  city: string;
+  number: number;
+  complement: string;
+  district: string;
+  state: string;
+  postalCode: string;
+
+  constructor(
+    id: number,
+    street: string,
+    city: string,
+    number: number,
+    complement: string,
+    district: string,
+    state: string,
+    postalCode: string
+  ) {
+    this.id = id;
+    this.street = street;
+    this.city = city;
+    this.number = number;
+    this.complement = complement;
+    this.district = district;
+    this.state = state;
+    this.postalCode = postalCode;
+  }
+}
---
+++ src/model/Cities.model.ts
@@ -0,0 +1,13 @@
+
+export class CitiesModel {
+    id: number;
+    name: string;
+
+    constructor (
+        id: number,
+        name: string,
+    ) {
+        this.id = id;
+        this.name = name;
+    }
+}
